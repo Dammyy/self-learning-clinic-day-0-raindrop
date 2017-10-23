7ed65d8cef225331205465caaf04ca49b6b10cd7
Damilare Olatubosun <damilareolatubosun@gmail.com>
2017-10-23 21:29:25 +0100

    properly implemented 2 space indentation in code

--- raindrop.js
+++ raindrop.js
@@ -1,45 +1,39 @@
 "use strict"
 let primeNumber = require("./prime.js");
- module.exports = {
+module.exports = {
 
- raindrop: (number) => {
+  raindrop: (number) => {
     let strng = "";
-	const three =  "Pling";
-	const five  = "Plang";
-	const seven = "Plong";
-	 
-	if(!number) {
-		return "please enter a number";
-	}
-	else if(typeof(number) !='number') {
-		return "undefined";
-	}
-	else if(number < 1) {
-		return "invalid input";
-	}
-	else {
+    const three = "Pling";
+    const five = "Plang";
+    const seven = "Plong";
 
-	for(let i = 2; i < number; i++) {
-	
-		let prime = primeNumber.getPrimeNumber(i);
-		
-		if(number % prime === 0 && prime == 3) {
-			strng += three;
-		}
-		else if(number % prime === 0 && prime == 5) {
-			strng += five
-		}
-		else if(number % prime === 0 && prime == 7) {
-			strng += seven;
-		}
-	}
+    if (!number) {
+      return "please enter a number";
+    } else if (typeof(number) != 'number') {
+      return "undefined";
+    } else if (number < 1) {
+      return "invalid input";
+    } else {
 
-	if(!strng) {
-		return number;
-	}
+      for (let i = 2; i < number; i++) {
 
-	return strng;
+        let prime = primeNumber.getPrimeNumber(i);
+
+        if (number % prime === 0 && prime == 3) {
+          strng += three;
+        } else if (number % prime === 0 && prime == 5) {
+          strng += five
+        } else if (number % prime === 0 && prime == 7) {
+          strng += seven;
+        }
+      }
+
+      if (!strng) {
+        return number;
+      }
+
+      return strng;
+    }
+  }
 }
-}
-}
-
--- test.js
+++ test.js
@@ -1,65 +1,65 @@
-const assert =  require('chai').assert;
+const assert = require('chai').assert;
 const rainObj = require('./raindrop.js');
 
 describe('raindrop', () => {
-	
-	describe('handle invalid input', () => {
-		it('should return undefined as the raindrop for demo', () => {
-			assert.equal(rainObj.raindrop('demo'), 'undefined');
-		});
-	}); 
-	
-	describe('handle invalid input', () => {
-		it('should return invalid input for 0', () => {
-			assert.equal(rainObj.raindrop(0), 'please enter a number');
-		});
-	});
-	
-	describe('handle valid input', () => {
-		it('should return PlingPlang for 15', () => {
-			assert.equal(rainObj.raindrop(15), 'PlingPlang');
-		});
-	});
-	
-		describe('handle valid input', () => {
-		it('should return plong for 28', () => {
-			assert.equal(rainObj.raindrop(28), 'Plong');
-		});
-	});
-	
-		describe('handle valid input', () => {
-		it('should return straight 1 for 1 ', () => {
-			assert.equal(rainObj.raindrop(1), '1');
-		});
-	});
-	
-		describe('handle valid input', () => {
-		it('should return Pling for 9', () => {
-			assert.equal(rainObj.raindrop(9), 'Pling');
-		});
-	});
-	
-	describe('handle valid input', () => {
-		it('should return PlingPlangPlong for 105', () => {
-			assert.equal(rainObj.raindrop(105), 'PlingPlangPlong');
-		});
-	}); 
-	describe('handle valid input', () => {
-		it('should return plong for 14', () => {
-			assert.equal(rainObj.raindrop(14), 'Plong');
-		});
-	}); 
-	
-		describe('handle invalid input', () => {
-		it('should return invalid input for -9', () => {
-			assert.equal(rainObj.raindrop(-9), 'invalid input');
-		});
-	}); 
-	
-		describe('handle valid input', () => {
-		it('should return straight 34 for 34', () => {
-			assert.equal(rainObj.raindrop(34), '34');
-		});
-	}); 
-	
+
+  describe('handle invalid input', () => {
+    it('should return undefined as the raindrop for demo', () => {
+      assert.equal(rainObj.raindrop('demo'), 'undefined');
+    });
+  });
+
+  describe('handle invalid input', () => {
+    it('should return invalid input for 0', () => {
+      assert.equal(rainObj.raindrop(0), 'please enter a number');
+    });
+  });
+
+  describe('handle valid input', () => {
+    it('should return PlingPlang for 15', () => {
+      assert.equal(rainObj.raindrop(15), 'PlingPlang');
+    });
+  });
+
+  describe('handle valid input', () => {
+    it('should return plong for 28', () => {
+      assert.equal(rainObj.raindrop(28), 'Plong');
+    });
+  });
+
+  describe('handle valid input', () => {
+    it('should return straight 1 for 1 ', () => {
+      assert.equal(rainObj.raindrop(1), '1');
+    });
+  });
+
+  describe('handle valid input', () => {
+    it('should return Pling for 9', () => {
+      assert.equal(rainObj.raindrop(9), 'Pling');
+    });
+  });
+
+  describe('handle valid input', () => {
+    it('should return PlingPlangPlong for 105', () => {
+      assert.equal(rainObj.raindrop(105), 'PlingPlangPlong');
+    });
+  });
+  describe('handle valid input', () => {
+    it('should return plong for 14', () => {
+      assert.equal(rainObj.raindrop(14), 'Plong');
+    });
+  });
+
+  describe('handle invalid input', () => {
+    it('should return invalid input for -9', () => {
+      assert.equal(rainObj.raindrop(-9), 'invalid input');
+    });
+  });
+
+  describe('handle valid input', () => {
+    it('should return straight 34 for 34', () => {
+      assert.equal(rainObj.raindrop(34), '34');
+    });
+  });
+
 });
