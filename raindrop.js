78397f17a1184f1cab1cab007c3b1ddad3170d4b
Damilare Olatubosun <damilareolatubosun@gmail.com>
2017-10-23 20:37:32 +0100

    add raindrop.js file

---
+++ raindrop.js
@@ -0,0 +1,54 @@
+"use strict"
+let primeNumber = require("./prime.js");
+ module.exports = {
+ getPrimeNumber: (number) => {
+	for(let i = 2; i < number - 1; i++) {
+		if(number % i === 0) {
+			return false;
+		}
+	}
+
+	return number;
+},
+
+ raindrop: (number) => {
+    let strng = "";
+	const three =  "Pling";
+	const five  = "Plang";
+	const seven = "Plong";
+	 
+	if(!number) {
+		return "please enter a number";
+	}
+	else if(typeof(number) !='number') {
+		return "undefined";
+	}
+	else if(number < 1) {
+		return "invalid input";
+	}
+	else {
+
+	for(let i = 2; i < number; i++) {
+	
+		let prime = primeNumber.getPrimeNumber(i);
+		
+		if(number % prime === 0 && prime == 3) {
+			strng += three;
+		}
+		else if(number % prime === 0 && prime == 5) {
+			strng += five
+		}
+		else if(number % prime === 0 && prime == 7) {
+			strng += seven;
+		}
+	}
+
+	if(!strng) {
+		return number;
+	}
+
+	return strng;
+}
+}
+}
+
